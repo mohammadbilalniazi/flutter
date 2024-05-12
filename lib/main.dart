cb1f8fd41d3d4fa44b64d33d180443c4eca15d93
mohammadbilalniazi <mohammadbilalniazi2016@gmail.com>
2024-05-12 09:15:15 +0430

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_app/pages/home.dart';
 
 void main() {
+  //function
+  // void is return Type of main
   runApp(const MyApp());
 }
 
@@ -10,14 +13,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // method
+    // Widget is return Type of build  and return type can be void int Widget(Component)
+    bringVegetable(thela: true);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 66, 58, 183)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
+  bringVegetable({@required bool thela: false, int rupees = 100}) {
+    //@required and required
+    print("thela $thela rupees $rupees");
+  }
+}
